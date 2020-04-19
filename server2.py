d2bfcd4f3d6800e1694e49d5086a777d43a2a0cf
jains8844 <jains8844@gmail.com>
2020-04-19 11:47:25 +0530

    use_extend_traces

--- server2.py
+++ server2.py
@@ -1,4 +1,4 @@
-import dash
+from dash import Dash
 import dash_core_components as dcc
 import dash_html_components as html
 from poliastro.examples import iss
@@ -6,13 +6,15 @@ from astropy import units as u
 from dash.dependencies import Input, Output, State
 from poliastro.plotting import OrbitPlotter3D
 from poliastro.bodies import Earth
+import dash_extendable_graph as deg
+
 # import plotly.graph_objects as go
-import flask
+# import flask
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__)
+app = Dash(__name__)
 
 server = app.server
 
@@ -23,23 +25,26 @@ app.layout = html.Div(children=[
         Dash: A web application framework for Python.
     '''),
 
-    dcc.Graph(figure = iss.plot(use_3d = True, interactive = True), id = "Figure"),
+    deg.ExtendableGraph(figure = iss.plot(use_3d = True, interactive = True), id = "Figure"),
     dcc.Interval(
         id='interval-component',
-        interval=2000, # in milliseconds
+        interval=1000, # in milliseconds
         n_intervals=0
     )
 ])
 
-@app.callback(Output('Figure', 'figure'), [Input('interval-component', 'n_intervals')], [State('Figure', 'figure')])
-def change_figure(n_intervals, figure):
+@app.callback(Output('Figure', 'extendData'), [Input('interval-component', 'n_intervals')])
+def change_figure(n_intervals):
 	r = iss.propagate(n_intervals*u.minute).r
 	plot = OrbitPlotter3D()
 	plot._attractor = Earth
 	figure1 = plot._plot_position(r, 'HEY', ['rgb(31, 119, 180)'])
-	figure['data'][2] = figure1.to_plotly_json()
-	figure['layout']['uirevision'] = 'False'
-	return figure
+	d1 = dict(figure1.to_plotly_json())
+	d2 = dict()
+	d2['x'] = d1['x']
+	d2['y'] = d1['y']
+	d2['z'] = d1['z']
+	return [d2], [2], 20
 
 if __name__ == '__main__':
     app.run_server(debug=True)
